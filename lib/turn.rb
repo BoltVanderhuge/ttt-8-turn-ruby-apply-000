38815391e24a59fe64aa2bb64b11ce872260df77
John Wisneski <john.j.wisneski@gmail.com>
2020-10-27 03:55:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,13 +21,14 @@ def position_taken? (board,index)
     
 end
 
-def input_to_index(input)
-  index = "#{input}".to_i - 1 
+def input_to_index(user_input)
+   user_input.to_i - 1
 end
 
-def move(board, index, value = "X")
-   board[index] = value
-  end
+def move(board, user_index, character = "X")
+  board[user_index] = character
+  board
+end
   
   def turn(board)
   puts "Please enter 1-9:"
