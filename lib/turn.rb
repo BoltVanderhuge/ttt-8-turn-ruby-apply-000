b23cd62efdd668090844431398434dfd9d1dbd7d
John Wisneski <john.j.wisneski@gmail.com>
2020-10-27 03:31:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,10 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move? (board,index)
-  
-  return board[index]== " "
-end
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    false
+    elsif index.between?(0,8) 
+    true
+  end
+
   
 
 def position_taken? (board,index)
